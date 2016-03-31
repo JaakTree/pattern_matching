165f2d609d2c19a03f59a9706805c4ee29d89cda
Andrey Rozumnyi <andrey.rozumnuy@gmail.com>
2016-03-31 08:32:46 +0300

    refactor and move process data script

--- Measurements/data/proc_data.py
+++ Measurements/data/proc_data.py
@@ -1,21 +1,23 @@
 from random import randint, choice
 
-def proc_data(in_file, out_file):
+
+def process_data(in_file, out_file):
     """ read chromosome genome and write genome without additional info to file """
     with open(in_file, 'r') as file:
-        data = file.readlines()
-        for i in range(1, len(data)):
-            data[i] = data[i].rstrip().replace("N", "")
+        genome = file.readlines()
+        for i in range(1, len(genome)):
+            genome[i] = genome[i].rstrip().replace("N", "")
 
     with open(out_file, "w") as file:
-        file.write(''.join(data[1:]))
+        file.write(''.join(genome[1:]))
 
-def read_data(file_name):
+
+def read_from(file_name):
     """ read all data from file """
     with open(file_name, 'r') as file:
         data = file.read()
-
     return data
+
 
 def write_lines_to(file_name, data, mode="w+"):
     """ write info to file line by line """
@@ -23,23 +25,25 @@ def write_lines_to(file_name, data, mode="w+"):
         for raw in data:
             file.write(str(raw) + "\n")
 
-def cut_pieces(data, size, number=20):
+
+def cut_pieces(data, size, number=25):
     """ cut fix sized pieces from genome """
     pieces = []
     data_len = len(data)
     for _ in range(number):
         index = randint(0, data_len-size-1)
         pieces.append(data[index:index+size])
-
     return pieces
 
-def write_pieces(data, start=5, end=30, step=5):
+
+def write_pieces(data, start=25, end=200, step=25):
     """ write cutted pieces of genome to according files """
     for size in range(start, end+1, step):
         pieces = cut_pieces(data, size)
         write_lines_to(str(size) + ".txt", pieces)
 
-def write_random_data(start=5, end=30, step=5, number=20):
+
+def write_random_data(start=25, end=200, step=25, number=25):
     """ generate random genome pieces of particular sizes. Append them to
      according files """
     alphabet = "ACGT"
@@ -51,9 +55,9 @@ def write_random_data(start=5, end=30, step=5, number=20):
 
         write_lines_to(str(size) + ".txt", raws, "a")
 
+
 if __name__ == "__main__":
-    pass
-    #proc_data("Homo_sapiens.GRCh37.68.dna.chromosome.1.fa", "processed.txt")
-    #data = _read_data("processed.txt")
-    #write_pieces(data)
-    #write_random_data()
+    #process_data("Homo_sapiens.GRCh37.68.dna.chromosome.1.fa", "processed.txt")
+    data = read_from("processed.txt")
+    write_pieces(data, 25, 200, 25)
+    write_random_data(25, 200, 25)
